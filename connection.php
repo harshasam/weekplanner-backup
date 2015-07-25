487e628b4d2029e8ac0c061376aaaaabf7826bc9
harshasam <harshasam@yahoo.com>
2015-07-26 00:38:48 +0100

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,152 @@
+<?php 
+//include ("db-connect.php"); 
+include ("functions.php");
+?>
+
+
+<?php 
+$mon1 = $_POST["mon1"];
+$tue1 = $_POST["tue1"];
+$wed1 = $_POST["wed1"];
+$thu1 = $_POST["thu1"];
+$fri1 = $_POST["fri1"];
+$sat1 = $_POST["sat1"];
+$sun1 = $_POST["sun1"];
+
+$mon2 = $_POST["mon2"];
+$tue2 = $_POST["tue2"];
+$wed2 = $_POST["wed2"];
+$thu2 = $_POST["thu2"];
+$fri2 = $_POST["fri2"];
+$sat2 = $_POST["sat2"];
+$sun2 = $_POST["sun2"];
+
+$mon3 = $_POST["mon3"];
+$tue3 = $_POST["tue3"];
+$wed3 = $_POST["wed3"];
+$thu3 = $_POST["thu3"];
+$fri3 = $_POST["fri3"];
+$sat3 = $_POST["sat3"];
+$sun3 = $_POST["sun3"];
+
+$mon4 = $_POST["mon4"];
+$tue4 = $_POST["tue4"];
+$wed4 = $_POST["wed4"];
+$thu4 = $_POST["thu4"];
+$fri4 = $_POST["fri4"];
+$sat4 = $_POST["sat4"];
+$sun4 = $_POST["sun4"];
+
+$mon5 = $_POST["mon5"];
+$tue5 = $_POST["tue5"];
+$wed5 = $_POST["wed5"];
+$thu5 = $_POST["thu5"];
+$fri5 = $_POST["fri5"];
+$sat5 = $_POST["sat5"];
+$sun5 = $_POST["sun5"];
+
+$mon6 = $_POST["mon6"];
+$tue6 = $_POST["tue6"];
+$wed6 = $_POST["wed6"];
+$thu6 = $_POST["thu6"];
+$fri6 = $_POST["fri6"];
+$sat6 = $_POST["sat6"];
+$sun6 = $_POST["sun6"];
+
+$mon7 = $_POST["mon7"];
+$tue7 = $_POST["tue7"];
+$wed7 = $_POST["wed7"];
+$thu7 = $_POST["thu7"];
+$fri7 = $_POST["fri7"];
+$sat7 = $_POST["sat7"];
+$sun7 = $_POST["sun7"];
+
+$mon8 = $_POST["mon8"];
+$tue8 = $_POST["tue8"];
+$wed8 = $_POST["wed8"];
+$thu8 = $_POST["thu8"];
+$fri8 = $_POST["fri8"];
+$sat8 = $_POST["sat8"];
+$sun8 = $_POST["sun8"];
+
+$mon9 = $_POST["mon9"];
+$tue9 = $_POST["tue9"];
+$wed9 = $_POST["wed9"];
+$thu9 = $_POST["thu9"];
+$fri9 = $_POST["fri9"];
+$sat9 = $_POST["sat9"];
+$sun9 = $_POST["sun9"];
+
+
+updateCellData("mon1",$mon1);
+updateCellData("mon2",$mon2);
+updateCellData("mon3",$mon3);
+updateCellData("mon4",$mon4);
+updateCellData("mon5",$mon5);
+updateCellData("mon6",$mon6);
+updateCellData("mon7",$mon7);
+updateCellData("mon8",$mon8);
+updateCellData("mon9",$mon9);
+
+updateCellData("tue1",$tue1);
+updateCellData("tue2",$tue2);
+updateCellData("tue3",$tue3);
+updateCellData("tue4",$tue4);
+updateCellData("tue5",$tue5);
+updateCellData("tue6",$tue6);
+updateCellData("tue7",$tue7);
+updateCellData("tue8",$tue8);
+updateCellData("tue9",$tue9);
+
+updateCellData("wed1",$wed1);
+updateCellData("wed2",$wed2);
+updateCellData("wed3",$wed3);
+updateCellData("wed4",$wed4);
+updateCellData("wed5",$wed5);
+updateCellData("wed6",$wed6);
+updateCellData("wed7",$wed7);
+updateCellData("wed8",$wed8);
+updateCellData("wed9",$wed9);
+
+updateCellData("thu1",$thu1);
+updateCellData("thu2",$thu2);
+updateCellData("thu3",$thu3);
+updateCellData("thu4",$thu4);
+updateCellData("thu5",$thu5);
+updateCellData("thu6",$thu6);
+updateCellData("thu7",$thu7);
+updateCellData("thu8",$thu8);
+updateCellData("thu9",$thu9);
+
+updateCellData("fri1",$fri1);
+updateCellData("fri2",$fri2);
+updateCellData("fri3",$fri3);
+updateCellData("fri4",$fri4);
+updateCellData("fri5",$fri5);
+updateCellData("fri6",$fri6);
+updateCellData("fri7",$fri7);
+updateCellData("fri8",$fri8);
+updateCellData("fri9",$fri9);
+
+updateCellData("sat1",$sat1);
+updateCellData("sat2",$sat2);
+updateCellData("sat3",$sat3);
+updateCellData("sat4",$sat4);
+updateCellData("sat5",$sat5);
+updateCellData("sat6",$sat6);
+updateCellData("sat7",$sat7);
+updateCellData("sat8",$sat8);
+updateCellData("sat9",$sat9);
+
+updateCellData("sun1",$sun1);
+updateCellData("sun2",$sun2);
+updateCellData("sun3",$sun3);
+updateCellData("sun4",$sun4);
+updateCellData("sun5",$sun5);
+updateCellData("sun6",$sun6);
+updateCellData("sun7",$sun7);
+updateCellData("sun8",$sun8);
+updateCellData("sun9",$sun9);
+?>
+
+
